8dc9202151105547d6ae9417c5ed45b5d5f3ffe9
Kenneth W. Parker <47489615+DesignToolInvestor@users.noreply.github.com>
2024-01-05 15:07:02 -0500

    Moved the reading of the network to the library called Net

--- visualize.py
+++ visualize.py
@@ -1,41 +1,14 @@
 #
-# visualize.py
+# v i s u a l i z e . p y
 #
 # This file creat a graph of a network.
 #
 # Call as
 #   visualize <file_name>
 
-import scanf
+import Net
 import sys
 import matplotlib.pyplot as plot
-import numpy as np
-
-def ReadNet(fileName):
-    file = open(fileName, 'r')
-
-    line = file.readline()
-    nNode = scanf.scanf("%d", line)[0]
-
-    nodeL = []
-    for nodeNum in range(nNode):
-        line = file.readline()
-        node = scanf.scanf("%f, %f", line)
-        nodeL.append(node)
-
-    line = file.readline()
-    nLink = scanf.scanf("%d", line)[0]
-
-    linkL = []
-    for linkNum in range(nLink):
-        line = file.readline()
-        link = scanf.scanf("%d, %d", line)
-        linkL.append(link)
-
-    file.close()
-
-    return (nodeL, linkL)
-
 
 def GraphNet(net):
     # pattern match on net
@@ -70,5 +43,5 @@ if __name__ == '__main__':
     print(sys.argv)
     fileName = sys.argv[1]
 
-    net = ReadNet(fileName)
+    net = Net.ReadNet(fileName)
     GraphNet(net)
