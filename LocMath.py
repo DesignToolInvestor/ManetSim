16693c72813f81da78a36ff6e0a79419a4195662
Kenneth W. Parker <47489615+DesignToolInvestor@users.noreply.github.com>
2024-01-08 08:31:02 -0500

    Added len function

--- LocMath.py
+++ LocMath.py
@@ -9,6 +9,10 @@ import math
 def Sqr(num):
     return num*num
 
-# TODO:  Move to project math library
+
 def Dist(node1, node2):
     return math.sqrt(Sqr(node2[0] - node1[0]) + Sqr(node2[1] - node1[1]))
+
+
+def Len(vector):
+    return math.sqrt(Sqr(vector[0]) + Sqr(vector[1]))
