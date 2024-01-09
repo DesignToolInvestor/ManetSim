318bad58d14c538d5b8e03febd7e26f6adc85cdf
Kenneth W. Parker <47489615+DesignToolInvestor@users.noreply.github.com>
2024-01-09 10:37:18 -0500

    Ripped out the code for arcs when plotting directional networks, because it was too hard to see what was happening.

--- Visual.py
+++ Visual.py
@@ -47,12 +47,17 @@ def GraphBiNet(net):
 
 ###############################################################################
 # Directed networks
+
+#######################################
+# Arcs
+# Might be vestigial, but don't delete yet, hard to create and might need later
 def CircPoint(circDef, ang):
     rad,(xCent,yCent) = circDef
-    xCent + rad * math.cos(ang)
-    yCent + rad * math.sin(ang)
 
-    return [xCent,yCent]
+    x = xCent + rad * math.cos(ang)
+    y = yCent + rad * math.sin(ang)
+
+    return [x,y]
     
     
 def ArcPoint(arcDef, pathFrac):
@@ -70,7 +75,7 @@ def Arc(start,stop, maxOffSet,maxArc):
 
     xStart,yStart = start
     xStop,yStop = stop
-    cord = (xStop - xStart, yStop - yStart)
+    cord = [xStop - xStart, yStop - yStart]
     cordLen = LocMath.Len(cord)
     cordAng = math.atan2(cord[1], cord[0])
 
@@ -79,25 +84,25 @@ def Arc(start,stop, maxOffSet,maxArc):
     if (maxArcOffSet > maxOffSet):
         yArg = 4*cordLen*maxOffSet / (LocMath.Sqr(cordLen) + 4*LocMath.Sqr(maxOffSet))
         xArg = (LocMath.Sqr(cordLen) - 4*LocMath.Sqr(maxOffSet)) / (LocMath.Sqr(cordLen) + 4*LocMath.Sqr(maxOffSet))
-        arc = 2 * math.atan2(yArg,xArg)
+        arcAng = 2 * math.atan2(yArg,xArg)
     else:
-        arc = maxArc
+        arcAng = maxArc
 
     # compute R
-    rad = cordLen / math.sqrt(2*(1 - math.cos(arc)))
+    rad = cordLen / math.sqrt(2*(1 - math.cos(arcAng)))
 
     # compute center
-    startAngToCent = cordAng + (math.pi - arc)/2
+    startAngToCent = cordAng + (math.pi - arcAng)/2
     xCent = xStart + rad * math.cos(startAngToCent)
     yCent = yStart + rad * math.sin(startAngToCent)
 
     # compute ard
     angStart = math.atan2(yStart - yCent, xStart - xCent)
-    angStop = math.atan2(yStop - yCent, xStop - xCent)
-    thetas = LocUtil.Grid1(angStart,angStop, NUM_POINT)
+    angStop = angStart + arcAng
+    angs = LocUtil.Grid1(angStart,angStop, NUM_POINT)
 
-    x = list(map(lambda t: CircPoint(rad,(xCent,yCent)), thetas))
-    y = list(map(lambda t: CircPoint(rad,(xCent,yCent)), thetas))
+    points = list(map(lambda ang: CircPoint((rad,(xCent,yCent)), ang), angs))
+    x,y = LocUtil.UnZip(points)
 
     return ((x,y), (rad, (xCent,yCent), (angStart, angStop)))
 
@@ -105,41 +110,62 @@ def Arc(start,stop, maxOffSet,maxArc):
 def ArcArrow(arcDef, pathFrac, headWidth):
     ARROR_ANG = math.pi/3
     
-    rad,cent,(angStart, angStop) = arcDef
     point,ang = ArcPoint(arcDef, pathFrac)
 
     dir = ang + math.pi/2
-    headAngOut = dir + ARROR_ANG/2
-    headAngIn = dir - ARROR_ANG/2
+    headAngOut = dir - math.pi + ARROR_ANG/2
+    headAngIn = dir - math.pi - ARROR_ANG/2
 
     headLen = headWidth / math.tan(ARROR_ANG/2)
-    tailOut = point + headLen * [math.cos(headAngOut),math.sin(headAngOut)]
-    tailIn = point + headLen * [math.cos(headAngIn),math.sin(headAngIn)]
+    outVec = [headLen * math.cos(headAngOut), headLen * math.sin(headAngOut)]
+    tailOut = [point[0] + outVec[0], point[1] + outVec[1]]
+    inVec = [headLen * math.cos(headAngIn), headLen * math.sin(headAngIn)]
+    tailIn = [point[0] + inVec[0], point[1] + inVec[1]]
     
     return [[point,tailOut], [point,tailIn]]
     
-    
+
+#######################################
+# vistigual GraphDirNet section
+#     for linkNum in range(nLink):
+#         link = links[linkNum]
+#
+#         (x,y),arcDef = Arc(nodes[link[0]],nodes[link[1]], MAX_OFF_SET,MAX_ARC)
+#         ax.plot(x,y, color='blue')
+#
+#         lines = ArcArrow(arcDef, 2/3, 0.05)
+#         x = [[lines[0][0][0], lines[1][0][0]], [lines[0][1][0], lines[1][1][0]]]
+#         y = [[lines[0][0][1], lines[1][0][1]], [lines[0][1][1], lines[1][1][1]]]
+#         ax.plot(x,y, color='blue')
+#
+#         (x,y),ang = ArcPoint(arcDef, 1/3)
+#         ax.text(x,y, str(linkNum), va="center", ha="center")
+
 def GraphDirNet(dirNet):
+    MAX_OFF_SET = 0.05
+    MAX_ARC = math.pi/3
+
     # pattern match on dirNet
     nodes,links = dirNet
+    nLink = len(links)
 
     # setup plot
     fig,ax = plot.subplots()
-    # TODO: setting the size doesn't seem to be working
-    plot.figure(figsize = (9, 6.5))
+    # TODO: setting the size doesn't seem to be working ... kind of important bug
+    plot.figure(figsize=(9, 6.5))
     ax.set_aspect('equal')
 
     # plot links
-    for linkNum in range(nLinks):
+    for linkNum in range(nLink):
         link = links[linkNum]
-        (x,y),arcDef= Arc(nodes[link[0]],nodes[link[1]])
-        ax.plot(x,y, color='blue')
-
-        (x,y) = ArcArrow(arcDef, 0.6, 0.1)
+        
+        seg = [nodes[link[0]], nodes[link[1]]]
+        x = [seg[0][0], seg[1][0]]
+        y = [seg[0][1], seg[1][1]]
         ax.plot(x,y, color='blue')
         
-        (x,y) = ArcLoc(arcDef, 0.4)
-        ax.text(x,y, str(linkNum), backgroundcolor="w")
+        point = LocMath.Interp(seg, 2/3)
+        ax.text(point[0],point[1], str(linkNum), color="blue", ha="center", va="center")
 
     # Plot nodes.  Want point on top of lines, so they come after line.
     # TODO: Change to create points and then unzip them for plotting
