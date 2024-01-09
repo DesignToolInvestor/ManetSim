a405721f29eda790fb5ac5e04c075355417b436c
Kenneth W. Parker <47489615+DesignToolInvestor@users.noreply.github.com>
2024-01-09 10:35:34 -0500

    Added an UnZip function.

--- LocUtil.py
+++ LocUtil.py
@@ -13,3 +13,21 @@ def Index(table, index):
 def Grid1(start, stop, nPoint):
     len = stop - start
     return list(map(lambda k: start + len * (k / (nPoint - 1)), range(nPoint)))
+
+
+def UnZip(zip):
+    x = []
+    y = []
+
+    for elem in zip:
+        x.append(elem[0])
+        y.append(elem[1])
+
+    return (x,y)
+
+
+# Not completed; doesn't work
+def PointToXY(pointLines):
+    map(lambda line: UnZip(line), pointLines)
+
+    return []
