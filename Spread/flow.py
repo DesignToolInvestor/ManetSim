ec8b3322b3016b8b1adfe35894dc70f43ca1b653
Kenneth W. Parker <47489615+DesignToolInvestor@users.noreply.github.com>
2024-01-10 17:39:01 -0500

    Almost works, but gives bad answer.

--- Spread/flow.py
+++ Spread/flow.py
@@ -60,13 +60,14 @@ def Net2FanLink(net):
     
     
 if __name__ == '__main__':
-    ###########################
-    # read network
+    # parse command line
+    # TODO:  Switch to using argparse
     fileName = sys.argv[1]
     nFlow = int(sys.argv[2])
 
+    # read network
     net,direct = Net.ReadNet(fileName)
-    if ~direct:
+    if not direct:
         raise Exception("The network must be directional.")
 
     nodes,links = net
@@ -93,22 +94,81 @@ if __name__ == '__main__':
         node2LinkOut[nodeStart].append(linkNum)
         node2LinkIn[nodeStop].append(linkNum)
 
-    print(node2LinkOut)
-    print(node2LinkIn)
+    ###########################################################
+    # Setup the problem
 
     ###########################
-    # Setup the problem
-    linkVar = []
+    # define the variables and the parameter
+    # TODO:  This should be a vector rather than a list
+    linkFlow = []
     for k in range(nLink):
-        linkVar.append(cvxpy.Variable())
+        linkFlow.append(cvxpy.Variable())
 
-    flowRate = cvxpy.Pareto(nonneg=True)
+    flowRate = cvxpy.Parameter(nonneg=True)
 
+    ###########################
+    # Do constraints
     constraints = []
-    # Do flow constraints
-    for k in range(nFlow):
-        source,sink = flowEnds[k]
-        linkVars = Index(linkVar, node2LinkOut[source])
-        constraints.append(sum(linkVars) == flowRate)
-        # net sum of in flows
 
+    # constraints at the ends of the flows
+    for flowNum in range(nFlow):
+        source,sink = flowEnds[flowNum]
+
+        outLinks = LocUtil.Index(linkFlow, node2LinkOut[source])
+        constraints.append(sum(outLinks) == flowRate)
+
+        inLinks = LocUtil.Index(linkFlow, node2LinkIn[sink])
+        constraints.append(sum(inLinks) == flowRate)
+
+    # constraints for conservation of flow (except at ends of the flows)
+    for nodeNum in range(nNode):
+        if nodeNum not in flowEnds:
+            outLinks = LocUtil.Index(linkFlow, node2LinkOut[nodeNum])
+            inLinks = LocUtil.Index(linkFlow, node2LinkIn[nodeNum])
+            constraints.append(sum(inLinks) == sum(inLinks))
+
+    # constraints for not overloading the nodes -- non-sink or source nodes
+    for nodeNum in range(nNode):
+        if nodeNum not in flowEnds:
+            outLinks = LocUtil.Index(linkFlow, node2LinkOut[nodeNum])
+            constraints.append(sum(outLinks) <= 1)
+
+    # constraints for not overloading the nodes -- sink and source nodes
+    for flowNum in range(nFlow):
+        source,sink = flowEnds[flowNum]
+
+        outLinks = LocUtil.Index(linkFlow, node2LinkOut[source])
+        inLinks = LocUtil.Index(linkFlow, node2LinkIn[source])
+        constraints.append(sum(outLinks) + sum(inLinks) <= 1)
+
+        outLinks = LocUtil.Index(linkFlow, node2LinkOut[sink])
+        inLinks = LocUtil.Index(linkFlow, node2LinkIn[sink])
+        constraints.append(sum(outLinks) + sum(inLinks) <= 1)
+
+    # constraints for not overloading links
+    for link in linkFlow:
+        constraints.append(0 <= link)
+        constraints.append(link <= 1)
+
+    ###########################
+    # set up the problem
+    factors = []
+    for linkNum in range(nLink):
+        factors.append(linkCost[linkNum] * linkFlow[linkNum])
+    goal = cvxpy.Minimize(sum(factors))
+
+    prob = cvxpy.Problem(goal, constraints)
+
+    ###################################
+    # solve the problem
+    flowRate.value = 1/4
+    prob.solve()
+    print(f'Solution = {prob.status}')
+
+    ###################################
+    # print the solution
+    psudoEps = 1e-10;
+    for linkNum in range(nLink):
+        rate = linkFlow[linkNum].value
+        if rate > psudoEps:
+            print(f'{linkNum}: {rate}')
