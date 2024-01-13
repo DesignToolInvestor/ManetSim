20ad563b71e30102c8290e760447bbf8946e16f7
Kenneth W. Parker <47489615+DesignToolInvestor@users.noreply.github.com>
2024-01-13 16:45:47 -0500

    Substantial bug fixing. Passes level-0 test

--- Spread/flow.py
+++ Spread/flow.py
@@ -3,8 +3,8 @@
 #
 # This program solves the capacity problem for a fixed number of flows on a given network.
 #
-# The formulation is to randomly pick end-point for the specified number of flows and then setup the optimization
-# problem to be solved via a separate program.
+# The formulation is to randomly pick end-point for the specified number of flows and then set up the
+# optimization problem to be solved via a separate program.
 #
 # Call this as
 #   SingleStrand <net_file> -n=<num_flow>
@@ -41,34 +41,49 @@ def RandEnds(nNode, nPair):
 
     return result
 
-# TODO: consider rather this is a general representation that should be moved to the Net library
-def Net2FanLink(net):
-    nodes,links= net
-    nNodes = len(nodes)
-    
-    # initialize linkFan
-    linkFan = []
-    for k in range(nNodes):
-        linkFan.append([])
-    
-    # walk the links filling in linkFan
-    for k in range(links):
-        n0,n1 = nodes[k]
-        linkFan[n0].append(k)
-        linkFan[n1].append(k)
-    
-    # return results
-    return linkFan
-    
 
 ###############################################################################
-def OptFlow(net, flow):
+# trace functionality
+# TODO:  Make this an object that encapsulates it's state
+
+TRACE_ON = True
+
+def Trace(format, stream=None, link0=None, link1=None):
+    if TRACE_ON:
+        if stream is None:
+            print(format)
+        else:
+            assert(link0 is not None)
+
+            if type(stream) != list:
+                stream = [stream]
+            if type(link0) != list:
+                link0 = [link0]
+
+            streamChar = list(map(lambda num: chr(num + 65), stream))
+
+            name0 = ["{}{}".format(linkNum,let) for linkNum in link0 for let in streamChar]
+            nameStr0 = LocUtil.List2Str(name0)
+
+            if link1 is None:
+                print(format.format(nameStr0))
+            else:
+                if (link1 != None) and (type(stream) != list):
+                    link1 = [link1]
+
+                name1 = ["{}{}".format(linkNum, let) for linkNum in link1 for let in streamChar]
+                nameStr1 = LocUtil.List2Str(name1)
+                print(format.format(nameStr0,nameStr1))
+
+
+###############################################################################
+def OptFlow(net, stream):
     # parse parameters
     node,link = net
     nNode = len(node)
     nLink = len(link)
 
-    nFlow = len(flow)
+    nStream = len(stream)
 
     # compute link cost
     linkCost = LinkCost(net)
@@ -85,15 +100,10 @@ def OptFlow(net, flow):
         node2LinkOut[start].append(linkNum)
         node2LinkIn[stop].append(linkNum)
 
-    ###########################################################
-    # Setup the problem
-
     ###########################
     # define the variables and the parameter
     # TODO:  This should be a vector rather than a list
-    linkRate = []
-    for k in range(nLink):
-        linkRate.append(cvxpy.Variable())
+    linkRate = list([cvxpy.Variable() for j in range(nLink)] for k in range(nStream))
 
     flowRate = cvxpy.Parameter(nonneg=True)
 
@@ -101,102 +111,138 @@ def OptFlow(net, flow):
     # Do constraints
     constraints = []
 
-    # constraints at the ends of the flows
-    for flowId in range(nFlow):
-        source, sink = flow[flowId]
+    # constraints at the ends of the streams
+    Trace("Constraints for sources and sinks")
+    for streamId in range(nStream):
+        source, sink = stream[streamId]
 
-        outLink = LocUtil.Index(linkRate, node2LinkOut[source])
-        print(f'{node2LinkOut[source]} = {flowRate}')
-        constraints.append(sum(outLink) == flowRate)
+        # source end of stream
+        outLink = node2LinkOut[source]
+        Trace('{} = flowRate', streamId, outLink)
+        constraints.append(sum(LocUtil.Index(linkRate[streamId], outLink)) == flowRate)
 
-        inLink = LocUtil.Index(linkRate, node2LinkIn[sink])
-        print(f'{node2LinkIn[sink]} = {flowRate}')
-        constraints.append(sum(inLink) == flowRate)
+        inLink = node2LinkIn[source]
+        Trace('{} = 0', streamId, inLink)
+        constraints.append(sum(LocUtil.Index(linkRate[streamId], inLink)) == 0)
 
-    # constraints for conservation of flow (except at ends of the flows)
-    flowEnds = LocUtil.FlattenAll(flow)
-    for nodeNum in range(nNode):
-        if nodeNum not in flowEnds:
-            outLink = LocUtil.Index(linkRate, node2LinkOut[nodeNum])
-            inLink = LocUtil.Index(linkRate, node2LinkIn[nodeNum])
-            print(f'{node2LinkOut[nodeNum]} = {node2LinkIn[nodeNum]}')
-            constraints.append(sum(inLink) == sum(outLink))
+        # sink end of stream
+        inLink = node2LinkIn[sink]
+        Trace('{} = flowRate', streamId, inLink)
+        constraints.append(sum(LocUtil.Index(linkRate[streamId], inLink)) == flowRate)
 
-    # constraints for avoiding overloading the nodes -- neither sink nor source nodes
-    for nodeNum in range(nNode):
-        if nodeNum not in flowEnds:
-            outLink = LocUtil.Index(linkRate, node2LinkOut[nodeNum])
-            inLink = LocUtil.Index(linkRate, node2LinkIn[nodeNum])
+        outLink = node2LinkOut[sink]
+        Trace('{} = 0', streamId, outLink)
+        constraints.append(sum(LocUtil.Index(linkRate[streamId], outLink)) == 0)
 
-            temp = node2LinkOut[nodeNum] + node2LinkIn[nodeNum]
-            print(f'{temp} <= 1')
-            constraints.append(sum(outLink) + sum(inLink) <= 1)
+    # constraints for conservation of stream (except at ends of the flows)
+    Trace("Constraints for conservation at non-source and non-sink nodes")
+
+    endNode = LocUtil.FlattenAll(stream)
+    nonEndNode = LocUtil.ListMinus([nodeId for nodeId in range(nNode)], endNode)
+
+    for streamId in range(nStream):
+        streamVarL = linkRate[streamId]
+        for nodeNum in range(nNode):
+            if nodeNum not in stream[streamId]:
+                outLink = node2LinkOut[nodeNum]
+                inLink = node2LinkIn[nodeNum]
+                Trace('{} = {}', streamId, outLink, inLink)
+                constraints.append(
+                    sum(LocUtil.Index(streamVarL, outLink)) == sum(LocUtil.Index(streamVarL, inLink)))
 
     # constraints for not overloading the nodes -- sink and source nodes
-    for flowId in range(nFlow):
-        source,sink = flow[flowId]
+    Trace("Constraints for node linits at source and sink nodes")
+    for nodeNum in endNode:
+        outLink = node2LinkOut[nodeNum]
+        inLink = node2LinkIn[nodeNum]
+        Trace('{} <= 1', list(range(nStream)), outLink + inLink)
 
-        outLink = LocUtil.Index(linkRate, node2LinkOut[source])
-        inLink = LocUtil.Index(linkRate, node2LinkIn[source])
+        outVar = list(linkRate[s][l] for l in outLink for s in range(nStream))
+        inVar = list(linkRate[s][l] for l in inLink for s in range(nStream))
+        constraints.append(sum(outVar) + sum(inVar) <= 1)
 
-        temp = node2LinkOut[source] + node2LinkIn[source]
-        print(f'{temp} <= 1')
+    # constraints for avoiding overloading the nodes -- for non-end nodes
+    Trace("Constraints for node linits at non-source and non-sink nodes")
+    for nodeNum in nonEndNode:
+        outLink = node2LinkOut[nodeNum]
+        inLink = node2LinkIn[nodeNum]
+        Trace('{} <= 1', list(range(nStream)), outLink + inLink)
 
-        constraints.append(sum(outLink) + sum(inLink) <= 1)
+        outVar = list(linkRate[s][l] for l in outLink for s in range(nStream))
+        inVar = list(linkRate[s][l] for l in inLink for s in range(nStream))
+        constraints.append(sum(outVar) + sum(inVar) <= 1)
 
-        outLink = LocUtil.Index(linkRate, node2LinkOut[sink])
-        inLink = LocUtil.Index(linkRate, node2LinkIn[sink])
-        constraints.append(sum(outLink) + sum(inLink) <= 1)
+    # no negative flows
+    Trace("Constraints against negative flows")
+    for linkId in range(nLink):
+        for streamId in range(nStream):
+            Trace('0 <= {}', streamId, linkId)
+            constraints.append(0 <= linkRate[streamId][linkId])
 
     # constraints for not overloading links
-    for link in linkRate:
-        constraints.append(0 <= link)
-        constraints.append(link <= 1)
+    Trace("Constraints for link limits")
+    for linkId in range(nLink):
+        Trace('{} <= 1', list(range(nStream)), linkId)
+
+        var = list(linkRate[s][linkId] for s in range(nStream))
+        constraints.append(sum(var) <= 1)
 
     ###########################
     # set up the problem
     factors = []
-    for linkNum in range(nLink):
-        factors.append(linkCost[linkNum] * linkRate[linkNum])
+    for streamId in range(nStream):
+        for linkNum in range(nLink):
+            factors.append(linkCost[linkNum] * linkRate[streamId][linkNum])
     goal = cvxpy.Minimize(sum(factors))
 
     prob = cvxpy.Problem(goal, constraints)
 
     ###################################
     # solve the problem
-    flowRate.value = 1
+    flowRate.value = 3/4
     prob.solve()
     print(f'Solution = {prob.status}')
 
     ###################################
     # print the solution
-    psudoEps = 1e-10;
-    for linkNum in range(nLink):
-        rate = linkRate[linkNum].value
-        if rate > psudoEps:
-            print(f'{linkNum}: {rate}')
-
+    PSUDO_EPS = 1e-8;
+    N_COLUMN = 5;
+    
+    for streamId in range(nStream):
+        print(f'Stream {chr(streamId + 65)}:')
+        nActive = 0
+        for linkNum in range(nLink):
+            rate = linkRate[streamId][linkNum].value
+            if abs(rate) > PSUDO_EPS:
+                if (nActive % N_COLUMN) != 0:
+                    print(", ", end="")
+                nActive += 1
+                print(f'({linkNum}: {round(rate*24)})', end="")
+                if (nActive % N_COLUMN) == 0:
+                    print()
+        if (nActive % N_COLUMN) != 0:
+            print()
 
 
 ###############################################################################
 def ParseArgs():
     parser = argparse.ArgumentParser(
-        prog='flow',
-        description='This program will compute the path of the optimal flow'
+        prog='stream',
+        description='This program will compute the path of the optimal stream'
     )
 
     parser.add_argument('fileName', type=str)
     parser.add_argument('-n', type=int)
-    parser.add_argument('-flow', type=str)
+    parser.add_argument('-stream', type=str)
     parser.add_argument('-seed', type=int)
 
     args = parser.parse_args()
-    if (args.n == None) and (args.flow == None):
+    if (args.n == None) and (args.stream == None):
         raise Exception('Must either specify the number of flows (to be picked randomly) or the flows')
-    elif (args.n != None) and (args.flow != None):
+    elif (args.n != None) and (args.stream != None):
         raise Exception("Can't specify both the number of random flows and the flows")
 
-    return [args.fileName, args.n, args.flow, args.seed]
+    return [args.fileName, args.n, args.stream, args.seed]
 
 
 if __name__ == '__main__':
@@ -216,16 +262,16 @@ if __name__ == '__main__':
         seed = SetSeed(seed)
 
         # TODO:  Do choose without replacement
-        flow = []
+        stream = []
         for k in range(n):
             source = random.randint(0, nNode - 1)
             sink = random.randint(0, nNode - 1)
-            flow[k].append([source, sink])
+            stream[k].append([source, sink])
 
     else:
         assert(flowStr != None)
 
         flowStrList = flowStr.split("),")
-        flow = list(map(lambda str: scanf.scanf("(%d,%d", str), flowStrList))
+        stream = list(map(lambda str: scanf.scanf("(%d,%d", str), flowStrList))
 
-    OptFlow(net, flow)
+    OptFlow(net, stream)
