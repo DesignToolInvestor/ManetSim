eb79cb72dfb97c6d677d44486b92ce0a13ca685d
Kenneth W. Parker <47489615+DesignToolInvestor@users.noreply.github.com>
2024-01-15 15:58:16 -0500

    Code extracted from gen_rand so that it can be used my multiple programs

---
+++ Spread/MakeNet.py
@@ -0,0 +1,53 @@
+#
+# R a n d N e t . p y
+#
+
+import math
+import random
+
+def RandNodeCirc(n, maxRad) -> list[[float,float]]:
+    result = []
+    for i in range(n):
+        angle = random.random() * 2 * math.pi
+        radQ = random.random()
+        rad = math.sqrt(radQ * maxRad*maxRad)
+        x = rad * math.cos(angle)
+        y = rad * math.sin(angle)
+        result.append([x,y])
+    return result
+
+
+def FindBiLinksSlow(nodeLoc) -> list[int,int]:
+    n = len(nodeLoc)
+
+    link = []
+    for k in range(n):
+        for j in range(k+1,n):
+            if LocMath.Dist(nodeLoc[k],nodeLoc[j]) < 1:
+                link.append([k,j])
+
+    return link
+
+
+# TODO:  Think about rather links should be sorted or not.  Right now they are not.
+def FindDirLinksSlow(nodeLoc) -> list[int,int]:
+    n = len(nodeLoc)
+
+    link = []
+    for k in range(n):
+        for j in range(k+1,n):
+            if LocMath.Dist(nodeLoc[k],nodeLoc[j]) < 1:
+                link.append([k,j])
+                link.append([j,k])
+
+    return link
+
+def RandCircNet(n,r, dir=False) -> [list[[float,float], list[int,int]]]:
+    nodeLoc = RandNodeCirc(n, r)
+
+    if dir:
+        links = FindDirLinksSlow(nodeLoc)
+    else:
+        links = FindBiLinksSlow(nodeLoc)
+
+    return (nodeLoc, links)
