1348ad084c3843c6ba3249887fdcdb257f357926
Kenneth W. Parker <47489615+DesignToolInvestor@users.noreply.github.com>
2024-01-26 18:35:16 -0500

    Changed to have an explicit Flush method rather than an implicit flushing when deleted.
    The implicit flushing was not reliably working because the file system is sometimes deleted first

--- Spread/Log.py
+++ Spread/Log.py
@@ -7,30 +7,35 @@
 from datetime import datetime
 
 
+# TODO:  originally flushed on deletion, but sometimes the file system was "unloaded"
 class Log(object):
-    def __init__(self, fileName, delay, trace=False):
+    def __init__(self, fileName, epicLen, trace=False):
         self.fileName = fileName
-        self.delay = delay
+        self.epicLen = epicLen
         self.trace = trace
 
-        self.lastDump = datetime.now()
+        self.startTime = datetime.now()
+        self.flushEpic = 0
         self.buff = []
 
     def Log(self, line):
         self.buff.append(line)
-        if (datetime.now() - self.lastDump).total_seconds() > self.delay:
+
+        epic = (datetime.now() - self.startTime).total_seconds() / self.epicLen
+        assert(self.flushEpic <= epic)
+
+        if (self.flushEpic < epic):
             with open(self.fileName, mode='a') as file:
-                for line in self.buff:
-                    file.write(f'{line}\n')
-                if self.trace:
-                    file.write(f'--------------\n')
+                Log.Flush(self)
+            self.flushEpic = epic
 
-            self.buff.clear()
-            self.lastDump = datetime.now()
-
-    def __del__(self):
+    def Flush(self):
         with open(self.fileName, mode='a') as file:
             for line in self.buff:
                 file.write(f'{line}\n')
             if self.trace:
                 file.write(f'--------------\n')
+
+        self.buff.clear()
+        self.lastFlush = datetime.now()
+ 
