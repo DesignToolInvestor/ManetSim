abd0886780060a23210de2a9d5b049ac12c83a35
Kenneth W. Parker <47489615+DesignToolInvestor@users.noreply.github.com>
2024-02-04 11:51:18 -0500

    First checkin

---
+++ FixedPath/UnitTest/test_BestPath.py
@@ -0,0 +1,63 @@
+#
+# t e s t _ B e s t P a t h . p y
+#
+
+from matplotlib import pyplot as plot
+import random
+from unittest import TestCase
+
+from BestPath import BestPath
+from Dijkstra import DijkstraBack, DijkstraFor
+import Cost
+from LocUtil import SetSeed
+from MakeNet import RandNetCirc
+import MakeNet
+from Visual import GraphBiNet
+
+class Test(TestCase):
+    def test_best_path(self):
+        # constants
+        nNode = 30
+        rho = 2
+
+        numNet = 3
+        numDest = 6
+
+        seed = None
+        numSeedDig = 3
+
+        for _ in range(numNet):
+            # make network
+            r = MakeNet.R(nNode,rho)
+
+            seed = SetSeed(seed, numSeedDig)
+            print(f'Seed: {seed}')
+
+            net = RandNetCirc(nNode, r)
+            nodeLoc,linkL = net
+
+            source = random.randint(0, len(nodeLoc) - 1)
+
+            # visualize net
+            fig, ax = plot.subplots(figsize=(6.5, 6.5))
+            GraphBiNet(ax, net, True, True, nodeNum=[k for k in range(nNode)])
+            plot.title(f'N = {nNode}; Seed = {seed}')
+
+            fileName = f'best_path_{nNode}_{seed}_cost'
+            plot.savefig(fileName, dpi=200)
+            plot.show()
+
+            # build ground truth
+            linkCost = [Cost.R(nodeLoc[n0], nodeLoc[n1]) for (n0, n1) in linkL]
+            cost,back = DijkstraFor(net, source, linkCost)
+
+            # Do tests
+            posDest = [k for k in range(nNode)]
+            posDest.pop(source)
+            destL = random.sample(posDest, k=numDest)
+            print(f'srouce = {source}, dest = {destL}')
+
+            for dest in destL:
+                turePath = DijkstraBack(back, dest)
+                compPath = BestPath(net, source,dest, linkCost)
+                self.assertEqual(turePath,compPath)
