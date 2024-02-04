321d4ff4eff07113ff2d150577ffa8bd76b77e97
Kenneth W. Parker <47489615+DesignToolInvestor@users.noreply.github.com>
2024-02-04 11:47:07 -0500

    First checkin

---
+++ FixedPath/Dijkstra.py
@@ -0,0 +1,55 @@
+#
+# D i j k s t r a . p y
+#
+
+from collections import deque
+
+from Net import Net2FanLink
+
+def DijkstraFor(net, sourceId, linkCost):
+    # set up
+    nodeLoc,linkL = net
+    nNodes = len(nodeLoc)
+
+    neighborTab = Net2FanLink(net)
+
+    nodeCost = [float("inf") for _ in range(nNodes)]
+    back = [-1 for _ in range(nNodes)]
+
+    visited = [False for _ in range(nNodes)]
+    toDo = []
+
+    # source node
+    nodeCost[sourceId] = 0
+    visited[sourceId] = True
+    toDo = [sourceId]
+    back[sourceId] = -1
+
+    # do loop
+    while 0 < len(toDo):
+        currId = toDo.pop()
+        currCost = nodeCost[currId]
+        neighbor = neighborTab[currId]
+
+        for nodeId,linkId in neighbor:
+            newCost = currCost + linkCost[linkId]
+            if newCost < nodeCost[nodeId]:
+                nodeCost[nodeId] = newCost
+                back[nodeId] = currId
+
+                if not nodeId in toDo:
+                    toDo.append(nodeId)
+
+    return nodeCost, back
+
+
+def DijkstraBack(back, dest):
+    node = dest
+    path = deque()
+
+    while 0 <= back[node]:
+        path.appendleft(node)
+        node = back[node]
+    path.appendleft(node)
+
+    return list(path)
