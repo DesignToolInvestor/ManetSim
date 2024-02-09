b6c92bc672c16ba5c95d82ebbb483cb6dc730fa5
Kenneth W. Parker <47489615+DesignToolInvestor@users.noreply.github.com>
2024-02-09 13:09:42 -0500

    Moved gode for setting up independent subsets to a separate file.  Also moved solving the interference graph to a separate file.

--- FixedPath/frac_chrom.py
+++ FixedPath/frac_chrom.py
@@ -10,6 +10,8 @@ import cvxpy
 
 from LocMath import PowerSet, RealToFrac
 from LocUtil import Sub
+from IndependSlow import IndSubSet
+from FracChromNum import FracChromNum
 
 #######################################
 def ParseArgs():
@@ -30,6 +32,7 @@ if __name__ == "__main__":
     fileName = ParseArgs()
 
     # read graph
+    # TODO:  fix lack of code reuse
     with open(fileName, 'r') as file:
         first, *rest = file.readlines()
 
@@ -38,52 +41,16 @@ if __name__ == "__main__":
     print(f'link = {linkL}')
 
     # power set of nodes
-    indepSet = []
-    powerSet = PowerSet(nNode)
+    indSubSet = IndSubSet(nNode, linkL)
+    result = FracChromNum(nNode, indSubSet)
 
-    for set in powerSet:
-        if (0 < len(set)):
-            setLen = len(set)
+    nResult = len(result)
+    print(f'[', end='')
+    for i in range(nResult - 1):
+        print(f'({i}, {result[i].numerator}/{result[i].denominator})', end='')
+    print(f'({i}, {result[nResult].numerator}/{result[nResult].denominator})]')
 
-            i0 = 0
-            done = False
-            while (i0 < setLen) and not done:
-                i1 = i0 + 1
+    printRes = [f'({i}, {frac.numerator}/{frac.denominator}))'
+                for (i,frac) in enumerate(result) if frac != 0]
 
-                while (i1 < setLen) and not done:
-                    link = (set[i0], set[i1])
-                    if link in linkL:
-                        done = True
-                    i1 += 1
-
-                i0 += 1
-                
-            if not done:
-                indepSet.append(set)
-
-    print(indepSet)
-
-    # create constraint
-    nIndep = len(indepSet)
-    # TODO:  change to a vector
-    indepVar = [cvxpy.Variable() for _ in range(nIndep)]
-
-    const = []
-    for node in range(nNode):
-        index = [i for i in range(nIndep) if node in indepSet[i]]
-        print(f'index = {index}')
-        const.append(sum(Sub(indepVar,index)) >= 1)
-
-    for var in indepVar:
-        const.append(var >= 0)
-
-    goal = cvxpy.Minimize(sum(indepVar))
-
-    prob = cvxpy.Problem(goal, const)
-    prob.solve(solver=cvxpy.ECOS)
-
-    if (prob.status != cvxpy.OPTIMAL):
-        raise Exception("solver failure", prob.status)
-
-    result = [RealToFrac(float(var.value)) for var in indepVar]
-    print(result)
+    print(sum(result))
