3371783d50c59276f49987a5e3e3649499178cbc
Kenneth W. Parker <47489615+DesignToolInvestor@users.noreply.github.com>
2024-02-09 13:05:15 -0500

    added cost functions for exclusion radius and exclusion area

--- FixedPath/Cost.py
+++ FixedPath/Cost.py
@@ -2,6 +2,25 @@
 # C o s t . p y
 #
 
-from LocMath import Dist
+# This library contains functions that are used for defining the cost of links
+
+from LocMath import Dist, Sqr
+from Interfere import InterDist
+
+
 def R(loc0, loc1):
     return Dist(loc0, loc1)
+
+
+def ExcluR(loc0, loc1, gamma, snir):
+    dist = Dist(loc0, loc1)
+    cost = InterDist(dist, gamma, snir)
+
+    return cost
+
+
+def ExcluArea(loc0, loc1, gamma, snir):
+    dist = Dist(loc0, loc1)
+    cost = Sqr(InterDist(dist, gamma, snir))
+
+    return cost
