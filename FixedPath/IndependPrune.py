0d7aecf2e091767ca49bf7223bc91489a78adf99
Kenneth W. Parker <47489615+DesignToolInvestor@users.noreply.github.com>
2024-02-14 20:16:20 -0500

    Converted to using sets of tuples instead of list of list.  Significantly faster.

--- FixedPath/IndependPrune.py
+++ FixedPath/IndependPrune.py
@@ -2,17 +2,18 @@
 # I n d e p e n d P r u n e . p y
 #
 
-# indSubSet is indexed by the numer of links in the subset.  Each element is a list of
-# independent subsets of the links with that many links in the subset.
+# Each element in subBySize contains all the independent subsets of a fixed size (i.e.,
+# of size index plus one).
 
-from LocUtil import Flatten, ListMinus
+from LocUtil import Flatten
+
 
 def IndSubSet(graph):
     # parse arguments
     nNode, linkL = graph
 
     # do case for subset size == 1
-    indSubSet = [[[id] for id in range(nNode)]]
+    subBySize = [{id for id in range(nNode)}]
 
     # do case for subset size == 2
     level2 = []
@@ -20,39 +21,40 @@ def IndSubSet(graph):
         for n1 in range(n0 + 1, nNode):
             # operation is important for speed
             if ((n0,n1) not in linkL) and ((n1,n0) not in linkL):
-                level2.append([n0,n1])
+                level2.append((n0,n1))
 
-    indSubSet.append(level2)
+    subBySize.append({*level2})
 
     # do cases for larger subset sizes
-    subSetSize = 2
-    while 0 < len(indSubSet[subSetSize - 1]):
+    subSetSize = 3
+    while 0 < len(subBySize[subSetSize - 2]):
         level = []
-        for leaf in indSubSet[subSetSize - 1]:
+        for leaf in subBySize[subSetSize - 2]:
+            # because the subsets are in order only need to consider extension beyond mas
             for extendNode in range(max(leaf) + 1, nNode):
-                i = 0
+                dropIndex = 0
                 noConflict = True
 
-                while noConflict and (i < subSetSize):
-                    testCase = leaf.copy()
-                    testCase[i] = extendNode
+                while noConflict and (dropIndex < subSetSize - 1):
+                    # care to keep the subset sorted
+                    temp = \
+                        tuple(leaf[i] for i in range(dropIndex)) + \
+                        tuple(leaf[i] for i in range(dropIndex + 1, subSetSize - 1))
+                    mustHave = (*temp, extendNode)
 
-                    # TODO: compair the speed of assembling it in order with sorting
-                    testCase.sort()
-
-                    # TODO: compare the speed of binary search, dict, and list
-                    # this operation will dominate speed
-                    noConflict = (testCase in indSubSet[subSetSize - 1])
-                    i += 1
+                    noConflict = (mustHave in subBySize[subSetSize - 2])
+                    dropIndex += 1
 
                 if noConflict:
-                    # TODO: compair the timing of assembling it in order with sorting
-                    nextSet = leaf.copy()
-                    nextSet.append(extendNode)
-                    level.append(sorted(nextSet))
+                    # care to keep the subset sorted
+                    newSub = (*leaf, extendNode)
+                    level.append(newSub)
 
-        indSubSet.append(level)
+        subBySize.append({*level})
         subSetSize += 1
 
     # return all the different sizes
-    return Flatten(indSubSet,1)
+    result = Flatten(subBySize,1)
+    result = [[*s] if type(s) == tuple else [s] for s in result]
+
+    return result
