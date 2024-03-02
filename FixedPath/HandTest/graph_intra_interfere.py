5a0f976bf4c5c3a90dd37a02804ccbc2f6bbb74f
Kenneth W. Parker <47489615+DesignToolInvestor@users.noreply.github.com>
2024-03-01 21:04:38 -0500

    Significantly improved the plot

--- FixedPath/HandTest/graph_intra_interfere.py
+++ FixedPath/HandTest/graph_intra_interfere.py
@@ -12,10 +12,12 @@ from random import sample
 
 # local libraries
 from BestPath import BestPath
-import Cost
-from Cost import ExcluR, MetricArg
-from LocMath import Cent, Dist
-from LocUtil import SetSeed, Sub, UnZip
+from CliqueSlow import Clique as CliqueSlow
+from CliquePrune import Clique as CliquePrune
+from Cost import LinkR, MetricCostF
+from Interfere import InterDist, PathSelfInter
+from LocMath import Add, Cent, Diff, Dist, Interp1, Scale, Perp
+from LocUtil import SetEq, SetSeed, Sub, UnZip
 import MakeNet
 from MakeNet import RandNetCirc
 
@@ -23,8 +25,8 @@ from MakeNet import RandNetCirc
 ###############################################################
 def ParseArgs():
     parser = argparse.ArgumentParser(
-            prog="plot_sssf",
-            description='This script will plot the achievable capacity for SSSFs'
+        prog="plot_sssf",
+        description='This script will plot the achievable capacity for SSSFs'
     )
 
     parser.add_argument('folder', type=str)
@@ -41,7 +43,7 @@ def ParseArgs():
     args = parser.parse_args()
 
     # deal with metric
-    costF,metric = MetricArg(args.metric, args.gamma, args.snirDb)
+    costF,metric = MetricCostF(args.metric, args.gamma, args.snirDb)
 
     # deal with flow
     flow = eval(args.flow) if args.flow is not None else None
@@ -62,10 +64,58 @@ def Circle(cent, r, nPoint):
     return point
 
 
+###############################################################
+def PlotCirc(net, path, gamma,snir):
+    nodeLoc,link = net
+    nHops = len(path) - 1
+
+    # plot just the node along the path
+    pathLoc = Sub(nodeLoc, path)
+    x, y = UnZip(pathLoc)
+    plot.plot(x, y, 'o', color="darkblue", markersize=5, zorder=0)
+
+    # plot source and sink markers
+    x,y = nodeLoc[path[0]]
+    plot.plot(x, y, 's', color="limegreen", markersize=10, zorder=-2)
+
+    x,y = nodeLoc[path[nHops]]
+    plot.plot(x, y, 'o', color="limegreen", markersize=10, zorder=-2)
+
+    # plot links along the path
+    x,y = UnZip(pathLoc)
+    plot.plot(x, y, color="crimson", zorder=-1)
+
+    # label links
+    for i in range(nHops):
+        x,y = Cent((pathLoc[i], pathLoc[i + 1]))
+        plot.text(
+            x,y, str(i), color="black", ha="center", va="center",
+            bbox=dict(facecolor='white', edgecolor='none'), zorder=1)
+
+    # do circles for exclusion zone
+    reciverLoc = pathLoc[1:]
+    hopDist = [Dist(pathLoc[k], pathLoc[k + 1]) for k in range(nHops)]
+
+    excluR = [InterDist(dist, gamma, snir) for dist in hopDist]
+    circL = [Circle(cent, r, nPointCirc) for (cent, r) in zip(reciverLoc, excluR)]
+
+    for circ in circL:
+        plot.plot(*UnZip(circ), 'g--')
+        
+    # do circle diameters
+    for i in range(nHops):
+        linkDir = Diff(*pathLoc[i: i+2])
+        perpDir = Perp(linkDir)
+
+        a = Add(reciverLoc[i], Scale(excluR[i], perpDir))
+        b = Add(reciverLoc[i], Scale(-excluR[i], perpDir))
+
+        plot.plot([a[0], b[0]], [a[1],b[1]], 'k:')
+
+
 #######################################
 if __name__ == "__main__":
     # constants
-    seed = None
     numSeedDig = 2
 
     snirDb = 0
@@ -81,60 +131,73 @@ if __name__ == "__main__":
     print(f'seed = {seed}')
 
     # make net
-    rad = MakeNet.R(nNode, rho)
-    net = RandNetCirc(nNode, rad)
+    netR = MakeNet.NetR(nNode, rho)
+    net = RandNetCirc(nNode, netR)
     nodeLoc, linkL = net
 
     flow = sample([k for k in range(nNode)], k=2)
 
     # find best flow
-    linkCost = [Cost.R(nodeLoc[n0], nodeLoc[n1]) for (n0, n1) in linkL]
+    linkCost = [LinkR(nodeLoc[n0], nodeLoc[n1]) for (n0, n1) in linkL]
     path = BestPath(net, *flow, linkCost)
-    nHops = len(path) - 1 if path is not None else None
-    print(f'{nHops} hops')
 
-    # start new graph
-    fig, ax = plot.subplots(figsize=(6.5, 6.5))
-    ax.set_aspect('equal')
+    if path is None:
+        print (f'Seed = {seed}, path = None')
+    else:
+        nHops = len(path) - 1
+        print(f'{nHops} hops')
 
-    # plot just the node along the path
-    pathLoc = Sub(nodeLoc, path)
-    x, y = UnZip(pathLoc)
-    plot.plot(x, y, 'o', color="darkblue", markersize=5, zorder=0)
+        # start new plot
+        fig, ax = plot.subplots(figsize=(6.5, 6.5))
+        ax.set_aspect('equal')
 
-    # plot source and sink markers
-    x,y = nodeLoc[flow[0]]
-    plot.plot(x, y, 's', color="limegreen", markersize=10, zorder=-2)
+        ####################################
+        # plot path
+        snir = 10 ** (snirDb / 20)
+        PlotCirc(net, path, gamma,snir)
 
-    x,y = nodeLoc[flow[1]]
-    plot.plot(x, y, 'o', color="limegreen", markersize=10, zorder=-2)
+        # do annotation
+        annotate = f'N = {nNode}\nnHops = {nHops}\nseed = {seed}'
 
-    # plot links along the path
-    x,y = UnZip(pathLoc)
-    plot.plot(x, y, color="crimson", zorder=-1)
+        x = Interp1(*ax.get_xlim(), 0.05)
+        y = Interp1(*ax.get_ylim(), 0.95)
+        plot.text(x,y, annotate, ha='left', va='top', multialignment="left")
 
-    # do circles for exclusion zone
-    destLoc = pathLoc[1:]
-    snir = 10 ** (snirDb / 20)
+        ####################################
+        # interference graph
+        linkLink = PathSelfInter(net,path,gamma,snir)
+        print(linkLink)
 
-    excluR = [ExcluR(pathLoc[k], pathLoc[k+1], gamma, snir) for k in range(nHops)]
-    print(f'exR = {excluR}')
-    circL = [Circle(cent, r, nPointCirc) for (cent, r) in zip(destLoc, excluR)]
+        # put interference graph on the plot
+        n = len(linkLink)
+        if n == 0:
+            text = 'None'
+        else:
+            text = "".join(f'{linkLink[i]}\n' for i in range(n - 1)) + str(linkLink[n - 1])
 
-    for circ in circL:
-        plot.plot(*UnZip(circ), 'g--')
+        x = Interp1(*ax.get_xlim(), 0.05)
+        y = Interp1(*ax.get_ylim(), 0.05)
+        plot.text(x,y, text, ha='left', va='bottom', multialignment="left")
 
-    # annotate
-    plot.title(f'Intra Path Interference')
+        ####################################
+        # clique number
+        cliqueSlow = cliqueSlow((nHops,linkLink))
+        cliquePrune = cliquePrune((nHops, linkLink))
 
-    x = ax.get_xlim()
-    y = ax.get_ylim()
-    annotate = f'N = {nNode}\nnHops = {nHops}\nseed = {seed}'
-    plot.text(x[0],y[1], annotate, ha='left', va='top', fontsize=10, multialignment="left")
+        assert(SetEq(cliqueSlow, cliquePrune))
 
-    # save figure
-    assert(rho == 2.0)
-    fileName = f'Interfere/rings_{nNode}_2_{seed}_{metric[0]}.png'
-    plot.savefig(fileName, dpi=200)
+        chromNum = max(map(len,cliquePrune))
+        text = f'Num Clique = {len(cliquePrune)}\nChrom # = {chromNum}'
 
-    plot.show()
+        x = Interp1(*ax.get_xlim(), 0.95)
+        y = Interp1(*ax.get_ylim(), 0.95)
+        plot.text(x, y, text, ha='right', va='top')
+
+        # annotate
+        plot.title(f'Intra Path Interference')
+
+        # save figure
+        assert(rho == 2.0)
+        fileName = f'Interfere/rings_{nNode}_2_{seed}_{metric[0]}.png'
+        plot.savefig(fileName, dpi=200)
+        # plot.show()
