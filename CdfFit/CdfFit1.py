2606aa448263ac4e86fec29ad1bce19ba5ae916a
Kenneth W. Parker <47489615+DesignToolInvestor@users.noreply.github.com>
2024-08-11 17:30:49 -0400

    Seems like it might be working, but might be overfitting

--- CdfFit/CdfFit1.py
+++ CdfFit/CdfFit1.py
@@ -2,24 +2,22 @@
 #  C d f F i t 1 . p y
 #
 
+import sympy as sp
+import cvxpy as cp
+
 from math import exp, log
+from numpy import sinc
 
-from LocUtil import Sub
-
-
-def EstAsym(sampX, probMap):
-  sampZ = [probMap.Forward(x) for x in sampX]
+from LocUtil import Grid1, MinMax, Sub
 
 
-# TODO:  add scaling so as to minimize the RMS residual
-# TODO:  create a higher level function that maximizes n without overfitting
-def CdfFit1(sampX, probMap):
+def EstAsym(sampX, map_):
   # process arguments
   nSamp = len(sampX)
   sampSort = sorted(sampX)
 
   # map samples to z
-  sampZ = tuple(probMap.Forward(x) for x in sampSort)
+  sampZ = tuple(map_.Forward(x) for x in sampSort)
 
   # compute the quantile
   quant = [(k + 0.5)/nSamp for k in range(nSamp)]
@@ -29,10 +27,45 @@ def CdfFit1(sampX, probMap):
 
   leftIndex = range(nEnd)
   leftSampZ = Sub(sampZ, leftIndex)
-  rightLogQ = log(Sub(quant, leftIndex))
-  a1,a0 = ToLimProj(leftSampZ, rightLogQ)
+  leftLogQ = log(Sub(quant, leftIndex))
+  a1,a0 = ToLimProj(leftSampZ, leftLogQ)
 
   rightIndex = range(nSamp - nEnd - 1, nSamp)
-  b1,b0 = ToLimProj(Sub(sampSort, rightIndex), Sub(quant, rightIndex))
+  rightSampZ = Sub(sampZ, rightIndex)
+  rightLoqQ = log(1 - Sub(quant, leftIndex))
+  b1,b0 = ToLimProj(rightSampZ, rightLoqQ)
 
   return (a0,a1, b0,b1)
+
+
+def MleZ(sampX, map_, nSinc):
+  # process arguments
+  nSamp = len(sampX)
+  sampSort = sorted(sampX)
+
+  # Analize the map
+  zSym = map_.zSym
+  xSym = map_.xSym
+
+  jacobSym = map_.invSym.diff(zSym).factor()
+  jacob = sp.lambdify(zSym, jacobSym)
+
+  # map samples to z
+  sampZ = tuple(map_.Forward(x) for x in sampSort)
+
+  # desnity weight
+  weight = tuple(jacob(z) for z in sampZ)
+
+  # setup sinc point
+  sincVal = cp.Variable(nSinc)
+  zMin,zMax = MinMax(sampZ)
+
+  h = (zMax - zMin) / (nSinc - 1)
+  sincZ = Grid1(zMin,zMax, nSinc)
+
+  # setup cost function
+  logLike = lambda z: \
+    cp.log(sum(sv * sinc((z - sz) / h) for (sz, sv) in zip(sincZ, sincVal)))
+  objective = cp.Maximize(sum(logLike(z) * weight(z) for z in sampZ))
+
+
